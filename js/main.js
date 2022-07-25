d8eaf9ff78315dfcf9bf7d470e35e1c98ca57796
Ren Wang <t2125230508@gmail.com>
2022-07-26 01:49:50 +0800

    Add setDelimiter()

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-function imageResize(ele, maxHeight) {
+function resizeImg(ele, maxHeight) {
     const height = ele.height();
     const width = ele.width();
     const scale = maxHeight / height;
+
     ele.css("height", maxHeight);
     ele.css("width", width * scale);
 }
 
-function imagePreview() {
+function setPreview() {
     const xOffset = 25;
     const maxHeight = 200;
+
     $("a.preview").hover(
         function(ele) {
             $("body").append("<p id='preview'><img src='" + this.href + "'></p>");
@@ -17,10 +19,11 @@ function imagePreview() {
             $("#preview").remove();
         }
     );
+
     $("a.preview").mousemove(
         function(ele) {
-            if($("#preview img").height() > maxHeight) {
-                imageResize($("#preview img"), maxHeight);
+            if ($("#preview img").height() > maxHeight) {
+                resizeImg($("#preview img"), maxHeight);
             }
             const yOffset = -xOffset - $("#preview img").height() / 2;
             $("#preview").css("top", (ele.pageY + yOffset) + "px");
@@ -29,4 +32,27 @@ function imagePreview() {
     );
 }
 
-$(document).ready(function(){ imagePreview(); });
+function setDelimiter() {
+    const items = document.getElementsByClassName("items");
+    for (let i=0; i<items.length; i++) {
+        let spans = items[i].querySelectorAll(":scope > span");
+        for (let j=0; j<spans.length-1; j++) {
+            spans[j].innerHTML += " /";
+        }
+    }
+
+    const authors = document.getElementsByClassName("authors");
+    for (let i=0; i<authors.length; i++) {
+        let spans = authors[i].querySelectorAll(":scope > span");
+        for (let j=0; j<spans.length-1; j++) {
+            spans[j].innerHTML += ",";
+        }
+    }
+}
+
+$(document).ready(
+    function() {
+        setPreview();
+        setDelimiter();
+    }
+);
